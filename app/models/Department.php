3ff513c74b6a68bfe33b795dfd33da95c44bf7bd
OliveSerg <oliveserg@gmail.com>
2021-09-05 13:46:20 -0400

    Create single view for department

--- app/models/Department.php
+++ app/models/Department.php
@@ -3,8 +3,11 @@
 namespace models;
 
 use general\Model;
+use models\Employee;
 
 class Department extends Model {
+    private ?Employee $manager = null;
+    private ?array $locations = null;
 
     public function __construct() {
         $this->table = 'DEPARTMENT';
@@ -23,5 +26,27 @@ class Department extends Model {
             'Dname' => 'Department Name',
             'MGR_SSN' => 'Manager Ssn'
         ];
+    }
+
+    public function getManager() {
+        if (!$this->manager && $this->getData('MGR_SSN')) {
+            $this->manager = Employee::find($this->getData('MGR_SSN'));
+        }
+
+        return $this->manager;
+    }
+
+    public function getLocations() {
+        if (!$this->locations && $this->Dnumber) {
+            $this->query->setType('collection');
+            $this->locations = $this->query->join('inner', 'DEPT_LOCATIONS', 'DEPT_LOCATIONS.Dnumber = ' .  $this->getTable() . '.' . $this->attributes['primarykey'])
+                ->where($this->getTable() . '.' . $this->attributes['primarykey'], $this->Dnumber)
+                ->select('Dlocation');
+
+            for ($i = 0; $i < count($this->locations); $i++) {
+                $this->locations[$i] = $this->locations[$i]['Dlocation'];
+            }
+        }
+        return $this->locations;
     }
 }
